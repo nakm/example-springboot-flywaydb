a7b0b996084744d96a60071151338cd54d87c820
nakm <nak178@gmail.com>
2018-04-24 23:14:43 +0900

    inital commit

---
+++ src/main/java/jp/co/example/demo/ExampleSpringbootFlywaydbApplication.java
@@ -0,0 +1,12 @@
+package jp.co.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringbootFlywaydbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringbootFlywaydbApplication.class, args);
+	}
+}
